5cb7e5c2f91332283a30f848b3ab3e450bb1d5d7
jpkhawam <jeanpaulkhawam@protonmail.com>
2022-05-25 16:38:12 +0300

    Initial commit

---
+++ src/test/java/com/jpkhawam/finalprojectapi/FinalProjectBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jpkhawam.finalprojectapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalProjectBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
