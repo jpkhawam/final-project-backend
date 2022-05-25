0364947f8e80b93071e6db6db91e4d7ea578121e
jpkhawam <jeanpaulkhawam@protonmail.com>
2022-05-25 17:56:07 +0300

    added default values in database

---
+++ src/main/resources/db/migration/V0.4.0__default_values_insertion.sql
@@ -0,0 +1,93 @@
+INSERT INTO categories(id, category)
+VALUES (DEFAULT, 'Entertainment');
+INSERT INTO categories(id, category)
+VALUES (DEFAULT, 'Food');
+INSERT INTO categories(id, category)
+VALUES (DEFAULT, 'Education');
+INSERT INTO categories(id, category)
+VALUES (DEFAULT, 'Sports');
+INSERT INTO categories(id, category)
+VALUES (DEFAULT, 'Nature');
+INSERT INTO categories(id, category)
+VALUES (DEFAULT, 'Desserts');
+INSERT INTO categories(id, category)
+VALUES (DEFAULT, 'Other');
+
+INSERT INTO activity_types(id, activity_type)
+VALUES (DEFAULT, 'Museum');
+INSERT INTO activity_types(id, activity_type)
+VALUES (DEFAULT, 'Beach');
+INSERT INTO activity_types(id, activity_type)
+VALUES (DEFAULT, 'Fast-Food Restaurant');
+INSERT INTO activity_types(id, activity_type)
+VALUES (DEFAULT, 'Vegan Restaurant');
+INSERT INTO activity_types(id, activity_type)
+VALUES (DEFAULT, 'Restaurant');
+INSERT INTO activity_types(id, activity_type)
+VALUES (DEFAULT, 'Pool');
+INSERT INTO activity_types(id, activity_type)
+VALUES (DEFAULT, 'Park');
+INSERT INTO activity_types(id, activity_type)
+VALUES (DEFAULT, 'Natural Reserve');
+INSERT INTO activity_types(id, activity_type)
+VALUES (DEFAULT, 'Playground');
+INSERT INTO activity_types(id, activity_type)
+VALUES (DEFAULT, 'Camping Site');
+INSERT INTO activity_types(id, activity_type)
+VALUES (DEFAULT, 'Smoke-Free Coffee-shop');
+INSERT INTO activity_types(id, activity_type)
+VALUES (DEFAULT, 'Coffee-shop');
+INSERT INTO activity_types(id, activity_type)
+VALUES (DEFAULT, 'Ice Cream Shop');
+INSERT INTO activity_types(id, activity_type)
+VALUES (DEFAULT, 'Sweets Shop');
+INSERT INTO activity_types(id, activity_type)
+VALUES (DEFAULT, 'Mall');
+INSERT INTO activity_types(id, activity_type)
+VALUES (DEFAULT, 'Sports');
+INSERT INTO activity_types(id, activity_type)
+VALUES (DEFAULT, 'Theater');
+INSERT INTO activity_types(id, activity_type)
+VALUES (DEFAULT, 'Cinema');
+INSERT INTO activity_types(id, activity_type)
+VALUES (DEFAULT, 'Summer Resort');
+INSERT INTO activity_types(id, activity_type)
+VALUES (DEFAULT, 'Winter Resort');
+INSERT INTO activity_types(id, activity_type)
+VALUES (DEFAULT, 'Library');
+INSERT INTO activity_types(id, activity_type)
+VALUES (DEFAULT, 'Arcade');
+INSERT INTO activity_types(id, activity_type)
+VALUES (DEFAULT, 'Bowling');
+INSERT INTO activity_types(id, activity_type)
+VALUES (DEFAULT, 'Karting');
+INSERT INTO activity_types(id, activity_type)
+VALUES (DEFAULT, 'Paintball');
+INSERT INTO activity_types(id, activity_type)
+VALUES (DEFAULT, 'Farm');
+INSERT INTO activity_types(id, activity_type)
+VALUES (DEFAULT, 'Zoo');
+INSERT INTO activity_types(id, activity_type)
+VALUES (DEFAULT, 'Other');
+
+INSERT INTO special_needs(id, special_need)
+VALUES (DEFAULT, 'In Wheelchair');
+INSERT INTO special_needs(id, special_need)
+VALUES (DEFAULT, 'Physically Impaired');
+INSERT INTO special_needs(id, special_need)
+VALUES (DEFAULT, 'Visually Impaired');
+INSERT INTO special_needs(id, special_need)
+VALUES (DEFAULT, 'Hearing Impaired');
+INSERT INTO special_needs(id, special_need)
+VALUES (DEFAULT, 'Autism');
+INSERT INTO special_needs(id, special_need)
+VALUES (DEFAULT, 'Epilepsy');
+INSERT INTO special_needs(id, special_need)
+VALUES (DEFAULT, 'Asthma');
+
+INSERT INTO settings(id, setting)
+VALUES (DEFAULT, 'Indoors');
+INSERT INTO settings(id, setting)
+VALUES (DEFAULT, 'Outdoors');
+INSERT INTO settings(id, setting)
+VALUES (DEFAULT, 'Mixed');
