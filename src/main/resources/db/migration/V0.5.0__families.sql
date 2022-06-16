57b4d55428c2b1cbcb61621abc768b2f53bfae1f
jpkhawam <jeanpaulkhawam@protonmail.com>
2022-06-17 01:15:37 +0300

    renamed table column to match base domain

--- src/main/resources/db/migration/V0.5.0__families.sql
+++ src/main/resources/db/migration/V0.5.0__families.sql
@@ -1,8 +1,8 @@
 CREATE TABLE families
 (
-    id          UUID PRIMARY KEY DEFAULT (uuid_generate_v4()),
-    email       TEXT NOT NULL,
-    family_name TEXT NOT NULL,
-    city_id     UUID,
+    id      UUID PRIMARY KEY DEFAULT (uuid_generate_v4()),
+    email   TEXT NOT NULL,
+    name    TEXT NOT NULL,
+    city_id UUID,
     CONSTRAINT fk_families_city_id FOREIGN KEY (city_id) REFERENCES cities (id)
 );
