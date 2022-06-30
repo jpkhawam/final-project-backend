157632013426ff4aecac6f83d7f930d24634c2a3
jpkhawam <jeanpaulkhawam@tutanota.com>
2022-06-30 14:48:57 +0300

    added family_id param to family controller api

--- src/main/java/com/jpkhawam/finalprojectapi/apis/families/FamilyController.java
+++ src/main/java/com/jpkhawam/finalprojectapi/apis/families/FamilyController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/families")
@@ -23,15 +24,20 @@ public class FamilyController extends BaseController {
             @RequestParam(name = "orderColumn", required = false, defaultValue = "name") FamilyOrderColumn familyOrderColumn,
             @RequestParam(name = "orderDirection", required = false, defaultValue = "ascending") OrderDirection orderDirection,
             @RequestParam(name = "limit", required = false) Integer limit,
-            @RequestParam(name = "offset", required = false) Integer offset) {
+            @RequestParam(name = "offset", required = false) Integer offset,
+            @RequestParam(name = "family_id", required = false) UUID family_id) {
         ApiRequest apiRequest = new ApiRequest();
         apiRequest.setOrderColumn(familyOrderColumn);
         apiRequest.setOrderDirection(orderDirection);
         apiRequest.setLimit(limit);
         apiRequest.setOffset(offset);
+        apiRequest.setFamily_id(family_id);
 
         Map<String, Object> response = new HashMap<>();
-        response.put("count", this.familyService.getCount(apiRequest));
+        if (family_id == null)
+            response.put("count", this.familyService.getCount(apiRequest));
+        else
+            response.put("count", 1);
         response.put("families", this.familyService.getAll(apiRequest));
         return response;
     }
